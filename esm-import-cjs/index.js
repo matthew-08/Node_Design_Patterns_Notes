4aab39acc569539037f05b161836c0ed49ee4427
matthew <matthewjcrosby1@gmail.com>
2023-12-31 12:45:01 -0600

    more

--- esm-import-cjs/index.js
+++ esm-import-cjs/index.js
@@ -1,3 +1,30 @@
+import { errorMonitor } from 'events'
 import bar from './test/cjs-module.js'
 
 console.log(bar)
+process.on('uncaughtException', (err) => {
+    try {
+        
+    } catch (error) {
+        console.error(err)
+    }
+    process.exit(1)
+})
+
+const nestedFunc = () => {
+    throw new Error('hello')
+}
+
+const func = () => {
+    try {
+        nestedFunc()
+    } catch (error) {
+        console.error(error)
+    }
+    console.log(poop.fakeprop)
+}
+
+func()
+
+console.log('test')
+
