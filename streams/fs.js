95ff49f2188f9f800b058c75e3500a9a7da195a2
matthew <matthewjcrosby1@gmail.com>
2024-01-15 09:59:03 -0600

    more fs streams

--- streams/fs.js
+++ streams/fs.js
@@ -2,4 +2,4 @@ const fs = require('fs');
 
 const rs = fs.createReadStream('./source.txt');
 
-rs.on('data', (chunk) => console.log(`Data receieved: ${chunk.toString()}`));
+rs.pipe(fs.createWriteStream('./dest.txt'));
