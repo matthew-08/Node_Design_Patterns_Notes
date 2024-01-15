4f047a125c5efb2845653b11e1cbb8e6743c91bd
matthew <matthewjcrosby1@gmail.com>
2024-01-15 13:16:16 -0600

    stream testing

--- streams/fs.js
+++ streams/fs.js
@@ -1,5 +1,35 @@
 const fs = require('fs');
-
+const { Transform } = require('stream');
 const rs = fs.createReadStream('./source.txt');
+const { createServer } = require('http');
 
-rs.pipe(fs.createWriteStream('./dest.txt'));
+class TransformString extends Transform {
+  constructor(options, searchStr, replaceStr) {
+    super(options);
+    this.tail = '';
+    this.searchStr = searchStr;
+    this.replaceStr = replaceStr;
+    this.pieces = [];
+  }
+
+  _transform(chunk, encoding, callback) {
+    this.pieces = (this.tail + chunk).split(this.searchStr);
+    const tailLength = this.searchStr.length;
+    this.tail = this.pieces[this.pieces.length - 1].slice(-tailLength);
+
+    this.pieces[this.pieces.length - 1] = this.pieces[
+      thispieces.length - 1
+    ].slice(0, -tailLength);
+    this.push(this.pieces.join(this.replaceStr));
+    callback();
+  }
+
+  _flush(callback) {
+    this.push(this.tail);
+    callback();
+  }
+}
+
+rs.pipe(new TransformString({}, 'world', 'node.js')).pipe(
+  fs.createWriteStream('./dest.txt')
+);
